eed2420fd2029e5757bf31343eca6535fb9a67cd
lilDrochilo <115777990+lilDrochilo@users.noreply.github.com>
2023-03-04 12:59:49 +0700

    Add files via upload

---
+++ UP_oop/1.2recurs/1.2recurs/Program.cs
@@ -0,0 +1,51 @@
+﻿class UP
+{
+    static void uniqueCombination(int l, int sum, int target, List<int> allCandidates, List<int> newCandidates)
+    {
+        if (sum == target)
+        {
+            Console.Write("{");
+            for (int i = 0; i < allCandidates.Count; i++)
+            {
+                if (i != 0)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(allCandidates[i]);
+                if (i != allCandidates.Count - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("}");
+            return;
+        }
+        for (int i = l; i < newCandidates.Count; i++)
+        {
+            if (sum + newCandidates[i] > target)
+            {
+                continue;
+            }
+            if (i > l && newCandidates[i] == newCandidates[i - 1])
+            {
+                continue;
+            }
+            allCandidates.Add(newCandidates[i]);
+            uniqueCombination(i + 1, sum + newCandidates[i], target, allCandidates, newCandidates);
+            allCandidates.RemoveAt(allCandidates.Count - 1);
+        }
+    }
+    static void Combination(List<int> newCandidates, int target)
+    {
+        newCandidates.Sort();
+        List<int> allCandidates = new List<int>();
+        uniqueCombination(0, 0, target, allCandidates, newCandidates);
+    }
+    public static void Main()
+    {
+        int[] candidates = { 10, 1, 2, 7, 6, 1, 5 };
+        List<int> newCandidates = new List<int>(candidates);
+        int target = 8;
+        Combination(newCandidates, target);
+    }
+}
